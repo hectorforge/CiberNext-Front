12b4cfc56c095532d01e1debcacca3efa10bfec6
hectorforge <hector.hernandez.dev@gmail.com>
2025-08-19 21:05:42 -0500

    El guard ya filtra por roles correctamente , se podra ver por consola del navegador

--- src/app/core/guards/user-guard-guard.ts
+++ src/app/core/guards/user-guard-guard.ts
@@ -1,6 +1,14 @@
 import { CanActivateFn, Router } from '@angular/router';
 import { inject } from '@angular/core';
 import { CookieService } from 'ngx-cookie-service';
+import {jwtDecode} from 'jwt-decode';
+
+interface JwtPayload {
+  roles: { id: number; nombre: string }[];
+  sub: string;
+  exp: number;
+  iat: number;
+}
 
 export const userGuardGuard: CanActivateFn = (route, state) => {
   const cookieService = inject(CookieService);
@@ -11,8 +19,24 @@ export const userGuardGuard: CanActivateFn = (route, state) => {
     return false;
   }
 
-  const userRole = cookieService.get('userRole');
+  const token = cookieService.get('authToken');
+  let decoded: JwtPayload;
+
+  try {
+    decoded = jwtDecode<JwtPayload>(token);
+  } catch (error) {
+    console.error('Error decodificando el token:', error);
+    router.navigate(['/auth/login']);
+    return false;
+  }
+
+  const userRole = decoded.roles[0]?.nombre; // <-- Aqui ya obtengo el nombre del rol
   const requiredRoles = route.data?.['roles'] as string[];
+
+  // Debugs xd
+  console.log('Token decodificado:', decoded);
+  console.log('Este es el userRole que llego : ', userRole);
+  console.log('Este es el requiredRoles que llego : ', requiredRoles);
 
   if (requiredRoles && !requiredRoles.includes(userRole)) {
     router.navigate(['/unauthorized']);
