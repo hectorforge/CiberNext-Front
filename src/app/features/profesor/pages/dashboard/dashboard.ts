0f85c125b5c4b44b36299c48b3ba29165919fc4c
cesardaniel97 <cesardx19@gmail.com>
2025-08-29 10:53:46 -0500

    corrigido y funcionando editar y ver profesor con id user

--- src/app/features/profesor/pages/dashboard/dashboard.ts
+++ src/app/features/profesor/pages/dashboard/dashboard.ts
@@ -3,23 +3,33 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { Profesor } from '../../models/profesor';
 import { ProfesorService } from '../../services/profesor';
+import { AuthService } from '../../../../core/services/auth-service'; 
 
 @Component({
   selector: 'app-dashboard-teacher',
   templateUrl: './dashboards.html',
   standalone: true,
   imports: [CommonModule, RouterModule],
-  
   styles: []
 })
 export class DashboardTeacher implements OnInit {
   profesores: Profesor[] = [];
+  userId: number | null = null;
 
-  constructor(private profesorService: ProfesorService) {}
+  constructor(
+    private profesorService: ProfesorService,
+    private authService: AuthService // Inyectamos AuthService
+  ) {}
 
   ngOnInit(): void {
+    // Obtener el userId desde el token
+    const token: any = this.authService.decodeToken();
+    console.log('Decoded Token:', token); 
+    this.userId = token?.userId;
+
+    
     this.profesorService.listar().subscribe(data => {
       this.profesores = data;
     });
-}
+  }
 }
