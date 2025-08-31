c0aa2eb7f19e6a6162e47e18d01f897afeb62fa1
cesardaniel97 <cesardx19@gmail.com>
2025-08-30 21:36:48 -0500

    mejorando lo del profe

--- src/app/features/profesor/profesor.router.ts
+++ src/app/features/profesor/profesor.router.ts
@@ -1,6 +1,5 @@
 import { Routes } from '@angular/router';
 import { ListarProfesoresComponent } from './pages/listar-profesores/listar-profesores';
-//import { CrearProfesoresComponent } from './pages/crear-profesores/crear-profesores';
 import { EditarProfesorComponent } from './pages/editar-profesores/editar-profesores';
 import { VerProfesorComponent } from './pages/ver-profesores/ver-profesores';
 import { DashboardTeacher } from './pages/dashboard/dashboard';
@@ -8,7 +7,6 @@ import { DashboardTeacher } from './pages/dashboard/dashboard';
 export const routesi: Routes = [
   
   { path: '', component: ListarProfesoresComponent },
-  //{ path: 'nuevo', component: CrearProfesoresComponent },
   { path: 'editar/:id', component: EditarProfesorComponent },
   { path: 'ver/:id', component: VerProfesorComponent },
 ];
